d4d629e953cdd1de9720331e9dc39d5727175b78
productthuanktone <38697899+productthuanktone@users.noreply.github.com>
2018-12-09 23:51:33 +0700

    gui mail duyet hoa don

--- VTNN/src/main/java/com/nguyenvanthuan/controller/GioHangController.java
+++ VTNN/src/main/java/com/nguyenvanthuan/controller/GioHangController.java
@@ -2,11 +2,14 @@ package com.nguyenvanthuan.controller;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpSession;
 import javax.transaction.Transactional;
 
@@ -14,6 +17,7 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -76,7 +80,7 @@ public class GioHangController {
 	@Transactional
 	public String MuaHang(@RequestParam String tenkhachhang, @RequestParam int sodienthoai,
 			@RequestParam int hinhthucgiaohang, @RequestParam String diachigiaohang, @RequestParam String mota,
-			HttpSession httpSession, ModelMap modelMap) {
+			HttpSession httpSession, ModelMap modelMap) throws MessagingException {
 		System.out.println(tenkhachhang);
 		if (null != httpSession.getAttribute("giohang")) {
 			List<GioHang> listgiohang = (List<GioHang>) httpSession.getAttribute("giohang");
@@ -129,14 +133,54 @@ public class GioHangController {
 					System.out.println("Them TC");
 					httpSession.removeAttribute("giohang");
 					String thanhcong = "Đặt Hàng Thành Công!";
-					SimpleMailMessage message = new SimpleMailMessage();
-					message.setFrom("thuandev2019@gmail.com");
-					message.setTo("thuanb1401193@student.ctu.edu.vn");
-					message.setSubject("Subject");
-					message.setText("test send gmail using spring");
-					// sending message
-					mailSender2.send(message);
-					System.out.println("Sending text done!");
+					// Sent email start
+					MimeMessage mimeMessage = mailSender2.createMimeMessage();
+					MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+					SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+					String htmlMsg = "<div style='width:500px;     background-color: #97d6e859;'><h3>"
+							+ khachHang.getHOTEN() + " thân mến!</h3>"
+							+ "<p style='width:500px;'>Yêu cầu đặt hàng cho đơn hàng của bạn đã được tiếp nhận và đang chờ xử lý, thời gian đặt hàng là "
+							+ dt.format(date)
+							+ " với hình thức thanh toán là Thanh toán khi nhận hàng. Chúng tôi sẽ tiếp tục cập nhật với bạn về trạng thái tiếp theo của đơn hàng.</p>"
+							+ "<h2 style='text-align:center;'>Chi tiết đơn hàng</h2>"
+							+ "<table class=\"table\" style=\"border: 1px solid black; border-collapse: collapse; width: 500px\">\r\n"
+							+ "		  <thead class=\"thead-dark\">\r\n"
+							+ "			<tr style=\"background-color: #CDDC39;\">\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;width:30px;\">#</th>\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Tên sản phẩm</th>\r\n"
+							+ "			  <th scope=\"col\"style=\"border: 1px solid black;\">Số lượng</th>\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Giá</th>\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Thành tiền</th>\r\n"
+							+ "			</tr>\r\n" + "		  </thead>\r\n" + "		  <tbody>\r\n";
+					float tongtien = 0;
+					int stt = 1;
+					for (GioHang gioHang : listgiohang) {
+						htmlMsg = htmlMsg + "			<tr>\r\n"
+								+ "			  <th scope=\"row\" style=\"border: 1px solid black; width:5px;\">" + stt++
+								+ "</th>\r\n" + "			  <td style=\'border: 1px solid black;\'>"
+								+ gioHang.getTensanpham() + "</td>\r\n"
+								+ "			  <td style=\"border: 1px solid black;\">" + gioHang.getSoluong()
+								+ "</td>\r\n" + "			  <td style=\"border: 1px solid black;\">"
+								+ gioHang.getGia() + "</td>\r\n"
+								+ "			  <td style=\"border: 1px solid black;\">"
+								+ gioHang.getGia() * gioHang.getSoluong() + "</td>\r\n" + "			</tr>\r\n";
+						tongtien = tongtien + gioHang.getGia() * gioHang.getSoluong();
+					}
+					htmlMsg = htmlMsg + "<tr> " + "<td colspan='4'style='border: 1px solid black;'>" + "Tổng tiền: "
+							+ "</td>" + "<td style='border: 1px solid black; color:red;'>" + tongtien + " VNĐ</td>"
+							+ "</tr></tbody>" + "</table>";
+					Calendar calendar = Calendar.getInstance();
+
+					calendar.roll(calendar.DAY_OF_MONTH, 3);
+					SimpleDateFormat dt1 = new SimpleDateFormat("dd/MM/yyyy");
+					htmlMsg = htmlMsg + "<p>Đơn hàng sẽ được giao vào ngày " + dt1.format(calendar.getTime())
+							+ ".<br>Địa chỉ giao hàng của bạn: " + khachHang.getDIACHI() +".<hr></p>";
+			
+					mimeMessage.setContent(htmlMsg, "text/html; charset=UTF-8");
+					helper.setTo(khachHang.getEMAIL());
+					helper.setSubject("Doanh nghiệp Hương Lúa đã nhận đơn hàng của bạn #" + date.getTime() + "");
+					mailSender2.send(mimeMessage);
+					// sent email end
 					String tendangnhap1 = (String) httpSession.getAttribute("tendangnhap");
 					if (null != tendangnhap1) {
 						TaiKhoan taiKhoan1 = taikhoanSV.laytaikhoan(tendangnhap);
@@ -146,6 +190,7 @@ public class GioHangController {
 						modelMap.addAttribute("taikhoan", taiKhoan1);
 					}
 					modelMap.addAttribute("tc", thanhcong);
+
 				} else {
 					System.out.println("Them TB");
 					String thatbai = "Đặt Hàng không Thành Công!";
@@ -166,14 +211,41 @@ public class GioHangController {
 					System.out.println("Them TC");
 					httpSession.removeAttribute("giohang");
 					String thanhcong = "Đặt Hàng Thành Công!";
-					SimpleMailMessage message = new SimpleMailMessage();
-					message.setFrom("thuandev2019@gmail.com");
-					message.setTo("thuanb1401193@student.ctu.edu.vn");
-					message.setSubject("Subject");
-					message.setText("test send gmail using spring");
-					// sending message
-					mailSender2.send(message);
-					System.out.println("Sending text done!");
+					// Sent email start
+					MimeMessage mimeMessage = mailSender2.createMimeMessage();
+					MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+					String htmlMsg = "	<h1>Danh sách Sản Phẩm</h1>	<table class=\"table\" style=\"border: 1px solid black; border-collapse: collapse; width: 500px\">\r\n"
+							+ "		  <thead class=\"thead-dark\">\r\n"
+							+ "			<tr style=\"background-color: #CDDC39;\">\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;width:30px;\">#</th>\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Tên sản phẩm</th>\r\n"
+							+ "			  <th scope=\"col\"style=\"border: 1px solid black;\">Số lượng</th>\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Giá</th>\r\n"
+							+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Thành tiền</th>\r\n"
+							+ "			</tr>\r\n" + "		  </thead>\r\n" + "		  <tbody>\r\n";
+					float tongtien = 0;
+					int stt = 0;
+					for (GioHang gioHang : listgiohang) {
+						htmlMsg = htmlMsg + "			<tr>\r\n"
+								+ "			  <th scope=\"row\" style=\"border: 1px solid black; width:5px;\">" + stt++
+								+ "</th>\r\n" + "			  <td style=\'border: 1px solid black;\'>"
+								+ gioHang.getTensanpham() + "</td>\r\n"
+								+ "			  <td style=\"border: 1px solid black;\">" + gioHang.getSoluong()
+								+ "</td>\r\n" + "			  <td style=\"border: 1px solid black;\">"
+								+ gioHang.getGia() + "</td>\r\n"
+								+ "			  <td style=\"border: 1px solid black;\">"
+								+ gioHang.getGia() * gioHang.getSoluong() + "</td>\r\n" + "			</tr>\r\n";
+
+						tongtien = tongtien + gioHang.getGia() * gioHang.getSoluong();
+					}
+					htmlMsg = htmlMsg + "<tr> " + "<td colspan='4'style='border: 1px solid black;'>" + "Tổng tiền: "
+							+ "</td>" + "<td style='border: 1px solid black; color:red;'>" + tongtien + " VNĐ</td>"
+							+ "</tr></tbody>" + "</table>";
+					mimeMessage.setContent(htmlMsg, "text/html; charset=UTF-8");
+					helper.setTo("thuanb1401193@student.ctu.edu.vn");
+					helper.setSubject("Doanh nghiệp Hương Lúa đã nhận đơn hàng của bạn!");
+					mailSender2.send(mimeMessage);
+					// sent email end
 					String tendangnhap1 = (String) httpSession.getAttribute("tendangnhap");
 					if (null != tendangnhap1) {
 						TaiKhoan taiKhoan1 = taikhoanSV.laytaikhoan(tendangnhap);
@@ -183,6 +255,7 @@ public class GioHangController {
 						modelMap.addAttribute("taikhoan", taiKhoan1);
 					}
 					modelMap.addAttribute("tc", thanhcong);
+
 				} else {
 					System.out.println("Them TB");
 					String thatbai = "Đặt Hàng không Thành Công!";
--- VTNN/src/main/java/com/nguyenvanthuan/controller/HoaDonController.java
+++ VTNN/src/main/java/com/nguyenvanthuan/controller/HoaDonController.java
@@ -1,8 +1,15 @@
 package com.nguyenvanthuan.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import com.nguyenvanthuan.entity.ChiTietHoaDon;
+import com.nguyenvanthuan.entity.GioHang;
 import com.nguyenvanthuan.entity.HoaDon;
 import com.nguyenvanthuan.service.ChiTietHoaDonService;
 import com.nguyenvanthuan.service.ChiTietSanPhamService;
@@ -26,6 +34,9 @@ public class HoaDonController {
 	ChiTietHoaDonService chitiethoadonService;
 	@Autowired
 	ChiTietSanPhamService chitietsanphamservice;
+	@Autowired
+	private JavaMailSender mailSender2;
+
 	@GetMapping
 	public String getHoaDon(ModelMap modelMap) {
 		List<HoaDon> listhoadon = hoadonService.listHoaDon();
@@ -43,28 +54,73 @@ public class HoaDonController {
 	}
 
 	@GetMapping("duyenhoadon/{tinhtrang}/{id}")
-	public String XuLyHoaDon(ModelMap modelMap, @PathVariable int id, @PathVariable int tinhtrang) {
-		if (kiemtrasoluong(id)) {
-			List<ChiTietHoaDon> chiTietHoaDonlist = chitiethoadonService.listchitiethoadonid(id);
-			for (ChiTietHoaDon chiTietHoaDon : chiTietHoaDonlist) {
-				int soluongmoi=Math.abs(chiTietHoaDon.getSOLUONG() - chiTietHoaDon.getChiTietSanPham().getSOLUONG());
-				System.out.println(soluongmoi);
-				chitietsanphamservice.updatesoluongsanpham(chiTietHoaDon.getChiTietSanPham().getMACHITIETSANPHAM(), soluongmoi);
+	public String XuLyHoaDon(ModelMap modelMap, @PathVariable int id, @PathVariable int tinhtrang)
+			throws MessagingException {
+		if (tinhtrang == 1) {
+			if (kiemtrasoluong(id)) {
+				List<ChiTietHoaDon> chiTietHoaDonlist = chitiethoadonService.listchitiethoadonid(id);
+				for (ChiTietHoaDon chiTietHoaDon : chiTietHoaDonlist) {
+					int soluongmoi = Math
+							.abs(chiTietHoaDon.getSOLUONG() - chiTietHoaDon.getChiTietSanPham().getSOLUONG());
+					System.out.println(soluongmoi);
+					chitietsanphamservice.updatesoluongsanpham(chiTietHoaDon.getChiTietSanPham().getMACHITIETSANPHAM(),
+							soluongmoi);
+				}
+				hoadonService.SaveHoaDon(tinhtrang, id);
+				return "redirect:/hoadon/";
+			} else {
+				String hethang = "Số lượng trong kho không đủ đáp ứng.";
+				modelMap.addAttribute("hethang", hethang);
+				List<ChiTietHoaDon> chitiethoadons = chitiethoadonService.listchitiethoadonid(id);
+				HoaDon hoaDon = hoadonService.hoaDonid(id);
+				modelMap.addAttribute("listchitiet", chitiethoadons);
+				modelMap.addAttribute("hoadon", hoaDon);
+				return "chitiethoadon";
 			}
+		} else {
 			hoadonService.SaveHoaDon(tinhtrang, id);
-			List<HoaDon> listhoadon = hoadonService.listHoaDon();
-			modelMap.addAttribute("hoadon", listhoadon);
-			return "hoadon";
-		}
-		else {
-			String hethang="Số lượng trong kho không đủ đáp ứng.";
-			modelMap.addAttribute("hethang", hethang);
-			List<ChiTietHoaDon> chitiethoadons = chitiethoadonService.listchitiethoadonid(id);
 			HoaDon hoaDon = hoadonService.hoaDonid(id);
-			modelMap.addAttribute("listchitiet", chitiethoadons);
-			modelMap.addAttribute("hoadon", hoaDon);
-			return "chitiethoadon";
+			List<ChiTietHoaDon> chitiethoadons = chitiethoadonService.listchitiethoadonid(id);
+			MimeMessage mimeMessage = mailSender2.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+			SimpleDateFormat dt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String htmlMsg = "<div style='width:500px;     background-color: #97d6e859;'><h3>"
+					+ hoaDon.getKhachhang().getHOTEN() + " thân mến!</h3>"
+					+ "<p style='width:500px;'>Đơn hàng đã được hủy theo yêu cầu của bạn. Chi tiết sản phẩm được liệt kê bên dưới. "
+					+ "<h2 style='text-align:center;'>Chi tiết đơn hàng</h2>"
+					+ "<table class=\"table\" style=\"border: 1px solid black; border-collapse: collapse; width: 500px\">\r\n"
+					+ "		  <thead class=\"thead-dark\">\r\n"
+					+ "			<tr style=\"background-color: #CDDC39;\">\r\n"
+					+ "			  <th scope=\"col\" style=\"border: 1px solid black;width:30px;\">#</th>\r\n"
+					+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Tên sản phẩm</th>\r\n"
+					+ "			  <th scope=\"col\"style=\"border: 1px solid black;\">Số lượng</th>\r\n"
+					+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Giá</th>\r\n"
+					+ "			  <th scope=\"col\" style=\"border: 1px solid black;\">Thành tiền</th>\r\n"
+					+ "			</tr>\r\n" + "		  </thead>\r\n" + "		  <tbody>\r\n";
+			float tongtien = 0;
+			int stt = 1;
+			for (ChiTietHoaDon chiTietHoaDon : chitiethoadons) {
+				htmlMsg = htmlMsg + "			<tr>\r\n"
+						+ "			  <th scope=\"row\" style=\"border: 1px solid black; width:5px;\">" + stt++
+						+ "</th>\r\n" + "			  <td style=\'border: 1px solid black;\'>"
+						+ chiTietHoaDon.getChiTietSanPham().getSanpham().getTENSANPHAM() + "</td>\r\n"
+						+ "			  <td style=\"border: 1px solid black;\">" + chiTietHoaDon.getSOLUONG()
+						+ "</td>\r\n" + "			  <td style=\"border: 1px solid black;\">"
+						+ chiTietHoaDon.getChiTietSanPham().getGia().getGIA() + "</td>\r\n"
+						+ "			  <td style=\"border: 1px solid black;\">"
+						+ chiTietHoaDon.getChiTietSanPham().getGia().getGIA() * chiTietHoaDon.getSOLUONG() + "</td>\r\n"
+						+ "			</tr>\r\n";
+				tongtien = tongtien + chiTietHoaDon.getChiTietSanPham().getGia().getGIA() * chiTietHoaDon.getSOLUONG();
+			}
+			htmlMsg = htmlMsg + "<tr> " + "<td colspan='4'style='border: 1px solid black;'>" + "Tổng tiền: " + "</td>"
+					+ "<td style='border: 1px solid black; color:red;'>" + tongtien + " VNĐ</td>" + "</tr></tbody>"
+					+ "</table>";
+			mimeMessage.setContent(htmlMsg, "text/html; charset=UTF-8");
+			helper.setTo(hoaDon.getKhachhang().getEMAIL());
+			helper.setSubject("Sản phẩm thuộc đơn hàng đã được hủy trên hệ thống!");
+			mailSender2.send(mimeMessage);
 		}
+		return "redirect:/hoadon/";
 
 	}
 
@@ -74,7 +130,7 @@ public class HoaDonController {
 		for (ChiTietHoaDon chiTietHoaDon : chiTietHoaDonlist) {
 			if (chiTietHoaDon.getSOLUONG() > chiTietHoaDon.getChiTietSanPham().getSOLUONG()) {
 				tong = tong + 1;
-			} 
+			}
 		}
 		if (tong == 0) {
 			return true;
